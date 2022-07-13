7d58a033069089dcf89febbd6e58b9388d9b9a9e
leprokuda <leprokuda@gmail.com>
2022-07-13 20:23:31 +0700

    Адаптив под все устройства + меню с бургером

--- src/js/index.js
+++ src/js/index.js
@@ -28,3 +28,45 @@
 //     }
 //   })
 // })
+
+
+
+// Выпадающее меню при клике на бургер
+
+const iconBurger = document.querySelector('.header__burger')
+
+if (iconBurger) {
+  const menuNav = document.querySelector('.header__nav')
+  iconBurger.addEventListener("click", function(e) {
+    document.body.classList.toggle('_lock')
+    iconBurger.classList.toggle('_active')
+    menuNav.classList.toggle('_active')
+  })
+}
+
+
+
+
+// Прокрутка к разделу при клике раздела в меню
+
+const menuLinks = document.querySelectorAll('.header__link[data-goto]')
+
+if (menuLinks.length > 0) {
+  menuLinks.forEach(menuLink => {
+    menuLink.addEventListener("click", onMenuLinkClick)
+  })
+
+  function onMenuLinkClick(e) {
+    const menuLink = e.target
+    if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+      const gotoBlock = document.querySelector(menuLink.dataset.goto)
+      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth"
+      })
+      e.preventDefault()
+    }
+  }
+}
